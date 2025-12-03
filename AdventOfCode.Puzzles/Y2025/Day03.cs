954f501c40032f05a1b6e96053aee1adbec76a0e
Ondřej Kunc <Ondrej.Kunc@optimidoc.com>
2025-12-03 07:32:10 +0100

    Y2025 D03

---
+++ AdventOfCode.Puzzles/Y2025/Day03.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode.Puzzles.Y2025;
+
+[AocPuzzle(2025, 3, "Lobby")]
+public class Day03 : IDay<long>
+{
+    public long Part1(ReadOnlySpan<char> span)
+        => PartCore(span, 2);
+
+    public long Part2(ReadOnlySpan<char> span)
+        => PartCore(span, 12);
+
+    static long PartCore(ReadOnlySpan<char> span, int digits)
+    {
+        long totalJoltageOutput = 0;
+        Span<char> joltageOutput = stackalloc char[digits];
+        foreach (var line in span.EnumerateLines())
+        {
+            int pos = 0;
+            for (int i = 0; i < joltageOutput.Length; i++)
+            {
+                for (char num = '9'; num >= '0'; num--)
+                {
+                    var index = line.Slice(pos).IndexOf(num);
+                    if (index == -1 || line.Length - (pos + index) < joltageOutput.Length - i)
+                        continue;
+
+                    pos += index;
+                    joltageOutput[i] = line[pos++];
+                    break;
+                }
+            }
+            totalJoltageOutput += long.Parse(joltageOutput);
+        }
+        return totalJoltageOutput;
+    }
+}
