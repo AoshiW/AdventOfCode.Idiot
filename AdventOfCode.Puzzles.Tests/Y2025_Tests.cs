080d3135348647c3e6cb2537c217d10e6f004c40
AoshiW <ondru@LAPTOP-7K2AEST8>
2025-12-15 00:15:12 +0100

    Y25D11

--- AdventOfCode.Puzzles.Tests/Y2025_Tests.cs
+++ AdventOfCode.Puzzles.Tests/Y2025_Tests.cs
@@ -13,8 +13,8 @@ public class Y2025_Tests
     [TestMethod] public async Task Day06() => await Assert.TestAsync<Day06, long>(4364617236318, 9077004354241);
     [TestMethod] public async Task Day07() => await Assert.TestAsync<Day07, long>(1646, 32451134474991);
     [TestMethod] public async Task Day08() => await Assert.TestAsync<Day08, int>(105952, 975931446);
-    //[TestMethod] public async Task Day09() => await Assert.TestAsync<Day09, int>(default, default);
+    [TestMethod] public async Task Day09() => await Assert.TestAsync<Day09, long>(4750297200, default);
     //[TestMethod] public async Task Day10() => await Assert.TestAsync<Day10, int>(default, default);
-    //[TestMethod] public async Task Day11() => await Assert.TestAsync<Day11, int>(default, default);
+    [TestMethod] public async Task Day11() => await Assert.TestAsync<Day11, int>(662, default);
     //[TestMethod] public async Task Day12() => await Assert.TestAsync<Day12, int>(default, default);
 }
---
+++ AdventOfCode.Puzzles/Y2025/Day11.cs
@@ -0,0 +1,106 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AdventOfCode.Puzzles.Y2025;
+
+[AocPuzzle(2025, 11, "Reactor")]
+public class Day11 : IDay<int>
+{
+    public int Part1(ReadOnlySpan<char> span)
+    {
+        var dic = new Dictionary<string, List<string>>();
+        foreach (var line in span.EnumerateLines())
+        {
+            var i = line.IndexOf(':');
+            var key = line.Slice(0, i).ToString();
+
+            var values = new List<string>();
+            var subLine = line.Slice(i + 2);
+            foreach (var value in subLine.Split(" "))
+            {
+                values.Add(subLine[value].ToString());
+            }
+            dic[key] = values;
+        }
+
+        return Get("you", dic);
+    }
+
+    int Get(string key, Dictionary<string, List<string>> dic)
+    {
+        int s = 0;
+        foreach (var k in dic[key])
+        {
+            if (k is "out")
+            {
+                s++;
+            }
+            else
+                s += Get(k, dic);
+        }
+        return s;
+    }
+
+    public int Part2(ReadOnlySpan<char> span)
+    {
+        //span = """
+        //    svr: aaa bbb
+        //    aaa: fft
+        //    fft: ccc
+        //    bbb: tty
+        //    tty: ccc
+        //    ccc: ddd eee
+        //    ddd: hub
+        //    hub: fff
+        //    eee: dac
+        //    dac: fff
+        //    fff: ggg hhh
+        //    ggg: out
+        //    hhh: out
+        //    """;
+        var dic = new Dictionary<string, List<string>>();
+        foreach (var line in span.EnumerateLines())
+        {
+            var i = line.IndexOf(':');
+            var key = line.Slice(0, i).ToString();
+
+            var values = new List<string>();
+            var subLine = line.Slice(i + 2);
+            foreach (var value in subLine.Split(" "))
+            {
+                values.Add(subLine[value].ToString());
+            }
+            dic[key] = values;
+        }
+
+        var hs = new List<string>();
+        return 1 * Get("svr", "you", dic, hs)
+            * Get("fft", "dac", dic, hs)
+            * Get("dac", "out", dic, hs)
+            * Get("svr", "fft", dic, hs)
+            ;
+    }
+
+    int Get(string key, string end, Dictionary<string, List<string>> dic, List<string> h)
+    {
+        if (h.Contains(key))
+            return 0;
+        h.Add(key);
+        var level = h.Count;
+        int s = 0;
+        if (!dic.TryGetValue(key, out var values))
+            return 0;
+        foreach (var k in values)
+        {
+            if (k == end)
+            {
+                    s++;
+            }
+            else
+            {
+                CollectionsMarshal.SetCount(h, level);
+                s += Get(k, end, dic, h);
+            }
+        }
+        return s;
+    }
+}
